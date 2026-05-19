30db59db77e77c63e5bfa5b0b6219605d716db3d
Stefania-Claudia Anghel <a_claudia92@yahoo.com>
2026-05-19 18:21:38 +0300

    added TavilySearch tool from langchain_tavily integration

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 # from langchain_openai import ChatOpenAI
 from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 tavily = TavilyClient()
@@ -23,8 +24,8 @@ def search(query: str) -> dict[str, Any]:
     return tavily.search(query=query)
 
 
-llm = ChatOllama(model="gemma4", temperature=0.8)
-tools = [search]
+llm = ChatOllama(model="gpt-oss", temperature=0.3)
+tools = [search, TavilySearch()]
 
 agent = create_agent(model=llm, tools=tools)
 
@@ -33,14 +34,15 @@ def main():
     print("Hello from langchain-course!")
 
     result = agent.invoke(
-        {"messages": HumanMessage(content="What is the weather in Bucharest?")}
+        {"messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the "
+                                          "Bucharest area on linkedin and list their details")}
     )
 
     # What happened here - ReAct Agent Architecture in action -->
     # The first llm received the input, and decided what tool to use (from the ones available) and with which arguments,
     # then it called the search tool, then sent the result of the search tool to the llm again, given the initial user
-    # query, the first llm output and the search tool call with arguments and output. This second llm did not choose to
-    # execute a tool, but it chose to return the final answer, which was not exactly the output of the search tool.
+    # query, the first llm output and the search tool call with its arguments and output. This second llm did not choose
+    # to execute a tool, but it chose to return the final answer, which was not exactly the output of the search tool.
     # You can check all of this in langsmith, and see how the agent is working step by step.
     # AWESOME! :)
 
