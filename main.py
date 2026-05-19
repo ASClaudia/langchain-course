338dfacff4f6432aa215fcf032edde429945df5d
Stefania-Claudia Anghel <a_claudia92@yahoo.com>
2026-05-19 17:39:51 +0300

    added comment about the steps executed by the agent

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
@@ -19,7 +19,7 @@ def search(query: str) -> str:
     return "Bucharest weather is sunny"
 
 
-llm = ChatOllama(model="gpt-oss")
+llm = ChatOllama(model="gemma4", temperature=0.8)
 tools = [search]
 
 agent = create_agent(model=llm, tools=tools)
@@ -31,6 +31,14 @@ def main():
     result = agent.invoke(
         {"messages": HumanMessage(content="What is the weather in Bucharest?")}
     )
+
+    # What happened here - ReAct Agent Architecture in action -->
+    # The first llm received the input, and decided what tool to use (from the ones available) and with which arguments,
+    # then it called the search tool, then sent the result of the search tool to the llm again, given the initial user
+    # query, the first llm output and the search tool call with arguments and output. This second llm did not choose to
+    # execute a tool, but it chose to return the final answer, which was not exactly the output of the search tool.
+    # You can check all of this in langsmith, and see how the agent is working step by step.
+    # AWESOME! :)
 
     print(result)
 
